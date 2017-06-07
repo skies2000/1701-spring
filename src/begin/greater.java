b3879286587a42db8e22ad69bc3d5ed452f15a24
skies2000 <skies2000@naver.com>
2017-06-07 11:26:29 +0900

    0607-4

--- src/begin/greater.java
+++ src/begin/greater.java
@@ -1,5 +1,7 @@
 package begin;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class greater {
 
 	private String format;
@@ -7,18 +9,28 @@ public class greater {
 	public String greet(String guest){
 		return String.format(format, guest);
 		
-	}
+	} 
 
 	public void setFormat(String format) {
 		this.format = format;
 	}
 	
 	public static void main(String[] args){
-		greater g = new greater();
+		/*
+		 * 스프링의 환결 설정 파일인 app_context.xml 을
+		 * 사용함으로써 같은 의미로 처리됨
+		 * greater g = new greater();
 		g.setFormat("%s 안녕하세요");
-		String temp = g.greet("Spring");
+		*/
 		
+		GenericXmlApplicationContext ctx =
+				new GenericXmlApplicationContext("begin/app_context.xml");
+		greater g = ctx.getBean("greater",greater.class);
+		
+		
+		String temp = g.greet("Spring");
 		System.out.println(temp);
+		ctx.close();
 		
 	}
 }
