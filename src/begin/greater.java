0bbb209bd6c28dda1a36108575d879b9a034f191
skies2000 <skies2000@naver.com>
2017-06-07 10:51:07 +0900

    0607-3

--- src/begin/greater.java
+++ src/begin/greater.java
@@ -2,4 +2,23 @@ package begin;
 
 public class greater {
 
+	private String format;
+	
+	public String greet(String guest){
+		return String.format(format, guest);
+		
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	public static void main(String[] args){
+		greater g = new greater();
+		g.setFormat("%s 안녕하세요");
+		String temp = g.greet("Spring");
+		
+		System.out.println(temp);
+		
+	}
 }
