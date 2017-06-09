5fa81d8976d6f6306c7d28243bf6cb6221288c2a
skies2000 <skies2000@naver.com>
2017-06-09 10:23:52 +0900

    0609-1(수정)

--- src/aop_begin/ImpeCalculator.java
+++ src/aop_begin/ImpeCalculator.java
@@ -5,7 +5,7 @@ public class ImpeCalculator implements Calculator{
 	@Override
 	public long factorial(long num) {
 		long result = 1;
-		for(int i=1; i<=num;num++){
+		for(int i=1; i<=num;i++){
 			result *=i;
 		}
 		return result;
