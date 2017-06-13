2ded9a9170f4102750e9264fbfd913d2667820ba
skies2000 <skies2000@naver.com>
2017-06-13 14:20:10 +0900

    0613-2

---
+++ src/mvc/HelloController.java
@@ -0,0 +1,21 @@
+package mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello") //요청 url중 /hello 경로가 표시되었을 때
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false)
+		    String name){
+		
+		model.addAttribute("greeting","안녕하세요"+name);
+		
+		return "hello"; // view의 이름.
+		
+	}
+}
